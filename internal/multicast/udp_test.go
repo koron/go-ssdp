064daba6e8d29b9747bf96161c73a57687b2c5b4
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-06-28 00:57:40 +0900

    fix multicast listen address

--- internal/multicast/udp_test.go
+++ internal/multicast/udp_test.go
@@ -3,13 +3,16 @@ package multicast
 import "testing"
 
 func TestSetMulticastRecvAddrIPv4(t *testing.T) {
+	// resolve with default
 	_, err := RecvAddrResolver.resolve()
 	if err != nil {
 		t.Errorf("resolve #1 failed: %s", err)
 	}
-	SetRecvAddrIPv4("224.0.0.0:1900")
-	if RecvAddrResolver.Addr != "224.0.0.0:1900" {
-		t.Errorf("unexpected RecvAddrResolver.Addr:\nwant=%q got=%q", "224.0.0.0:1900", RecvAddrResolver.Addr)
+
+	// resolve after override
+	SetRecvAddrIPv4("224.0.0.1:1900")
+	if RecvAddrResolver.Addr != "224.0.0.1:1900" {
+		t.Errorf("unexpected RecvAddrResolver.Addr:\nwant=%q got=%q", "224.0.0.1:1900", RecvAddrResolver.Addr)
 	}
 	_, err = RecvAddrResolver.resolve()
 	if err != nil {
@@ -18,16 +21,28 @@ func TestSetMulticastRecvAddrIPv4(t *testing.T) {
 }
 
 func TestSetMulticastSendAddrIPv4(t *testing.T) {
-	_, err := sendAddrResolver.resolve()
+	// resolve with default
+	_, err := SendAddr()
 	if err != nil {
 		t.Errorf("resolve #1 failed: %s", err)
 	}
+
+	// resolve after override
 	SetSendAddrIPv4("239.255.255.250:1900")
 	if sendAddrResolver.Addr != "239.255.255.250:1900" {
 		t.Errorf("unexpected sendAddrResolver.Addr:\nwant=%q got=%q", "239.255.255.250:1900", sendAddrResolver.Addr)
 	}
-	_, err = sendAddrResolver.resolve()
+	first, err := SendAddr()
 	if err != nil {
 		t.Errorf("resolve #2 failed: %s", err)
+	}
+
+	// resolve by cache
+	second, err := SendAddr()
+	if err != nil {
+		t.Errorf("resolve #3 failed: %s", err)
+	}
+	if second != first {
+		t.Errorf("cache mismatch: first=%p second=%p", first, second)
 	}
 }
