fa9d3295352c38aa5dcc104267a1448f2f2d2137
chad <chad_lwm@hotmail.com>
2023-09-09 15:40:09 +0800

    example monitor support type

--- examples/monitor/monitor.go
+++ examples/monitor/monitor.go
@@ -8,9 +8,13 @@ import (
 	"github.com/koron/go-ssdp"
 )
 
+var st string
+
 func main() {
 	v := flag.Bool("v", false, "verbose mode")
 	h := flag.Bool("h", false, "show help")
+	flag.StringVar(&st, "st", "", "ST: Type")
+
 	flag.Parse()
 	if *h {
 		flag.Usage()
@@ -34,14 +38,20 @@ func main() {
 }
 
 func onAlive(m *ssdp.AliveMessage) {
+	if st != "" && st != m.Type { return }
+
 	log.Printf("Alive: From=%s Type=%s USN=%s Location=%s Server=%s MaxAge=%d",
 		m.From.String(), m.Type, m.USN, m.Location, m.Server, m.MaxAge())
 }
 
 func onBye(m *ssdp.ByeMessage) {
+	if st != "" && st != m.Type { return }
+
 	log.Printf("Bye: From=%s Type=%s USN=%s", m.From.String(), m.Type, m.USN)
 }
 
 func onSearch(m *ssdp.SearchMessage) {
+	if st != "" && st != m.Type { return }
+
 	log.Printf("Search: From=%s Type=%s", m.From.String(), m.Type)
 }
